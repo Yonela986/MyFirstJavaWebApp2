8c8a610f59e959ee59515afc08609fb2d6a89b24
Yonela986 <yoza.futho@gmail.com>
2020-08-11 23:35:33 +0200

    latest update

--- src/main/java/net/myfirst/webapp/App.java
+++ src/main/java/net/myfirst/webapp/App.java
@@ -1,9 +1,12 @@
 package net.myfirst.webapp;
 
+import org.jdbi.v3.core.Handle;
+import org.jdbi.v3.core.Jdbi;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import static spark.Spark.*;
@@ -13,8 +16,18 @@ import static spark.Spark.*;
 public class App {
         public static void main(String[] args){
 
+            String dbDiskURL = "jdbc:h2:file:./greetdb";
+             //String dbMemoryURL = "jdbc:h2:mem:greetdb";
+            Jdbi jdbi = Jdbi.create(dbDiskURL, "sa", "");
+
+             //get a handle to the database
+            Handle handle = jdbi.open();
+
+            //create the table if needed
+            handle.execute("create table if not exists greet ( id integer identity, name varchar(50), counter int )");
             port(getHerokuAssignedPort());
-            //List<String> usernames = new ArrayList<>();
+
+            //List<String> users = new ArrayList<>();
             Map<String, Integer> users = new HashMap<>();
 
             get("/greet/:name", (request, response) -> {
@@ -25,6 +38,10 @@ public class App {
             });
 
             get("/hello", (req, res) -> {
+
+                List<String> names = handle.createQuery("select name from greet")
+                        .mapTo(String.class)
+                        .list();
                 Map<String, Object> map = new HashMap<>();
 
                 map.put("users" , users);
@@ -41,36 +58,37 @@ public class App {
                 String greeting = ""  +  username;
 
               //  System.out.println(req.queryParams("language"));
+                        if(users.containsKey(username)){
+                            users.put(username, users.get(username)+1);}
+                            // add a user to the database
+                   //handle.execute("insert into greet (name) values (?)", username);
+                   else{ users.put(username,1);
+                            // check if a user exists
+                            int count = handle.select("select count(*) from greet where name = ?", username)
+                                    .mapTo(int.class)
+                                    .findOnly();
+                            if (count == 0){
+                                handle.execute("insert into greet (name) values (?)", username);
+                            }
 
-                if(users.containsKey(username)){
-                    users.put(username, users.get(username)+1);
-                }
-                else{
-                    users.put(username,1);
-                }
+                   //if the user wasn't greeted before
+//                   if (!users.containsKey(username)) {
+//                       users.add(username);
+//                   }
+               }
+                // get all the usernames from the database
+                List<String> names = handle.createQuery("select name from greet")
+                        .mapTo(String.class)
+                        .list();
+                //Map<String, Object> users = new HashMap<>();
                 // put it in the map which is passed to the template - the value will be merged into the template
-                map.put("greeting", greeting);
-                map.put("counter" , users.size());
-                map.put("users", users);
+                map.put("greeting", users);
+                map.put("counter" , names.size());
+                map.put("users", names);
 
                 return new HandlebarsTemplateEngine().render(new ModelAndView(map, "hello.handlebars"));
 
-                //create a Switch statement for selecting languages
-                /*String language = "language";
 
-                switch (language){
-                    case 1:
-                        System.out.println("Xhosa has been selected");
-                        break;
-                    case 2 :
-                        System.out.println("English has been selected");
-                        break;
-                    case 3:
-                        System.out.println("Afrikaans has been selected");
-                        break;
-                    default:
-                        System.out.println("correct language selected");
-                }*/
             });
 
         }
